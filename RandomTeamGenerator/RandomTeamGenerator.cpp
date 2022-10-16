136f4497fef1ace39c69ab0f3cbcf154972e774e
Cactus-John <ivan.ivo.bosnjak@gmail.com>
2022-10-16 22:44:58 +0200

    initial commit

--- RandomTeamGenerator/RandomTeamGenerator.cpp
+++ RandomTeamGenerator/RandomTeamGenerator.cpp
@@ -1,20 +1,54 @@
-// RandomTeamGenerator.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
+#include <algorithm>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+class randomTeamGenerator {
+
+    public:
+        int playerNumbers, teamNumbers;
+    private:
+        string players;
+
+    public:
+        randomTeamGenerator() {
+            players = "0";
+        }
+        
+    randomTeamGenerator(string players) {
+        this->players = players;
+    }
+
+    // user writes all the players in his team
+    void teamMembers() {
+        
+        string* players = new string[teamNumbers * playerNumbers];
+        for (int i = 0; i < teamNumbers * playerNumbers; i++) {
+            cin >> players[i];
+        }
+    }
+
+    void divideTeams() {
+        if (teamNumbers % 2 == 0) {
+            for (int i = 0; i < teamNumbers * playerNumbers; i++) {
+                players[i] = teamNumbers;
+                cout << players[i];
+            }
+        }
+    }
+};
+
+int main() {
+
+    randomTeamGenerator names;
+    cout << "Enter the number of teams: " << endl;
+    cin >> names.teamNumbers;
+    cout << "Enter the number of players: " << endl;
+    cin >> names.playerNumbers;
+    system("CLS");
+    cout << "Write the names of the players: " << endl; 
+    names.teamMembers();
+    names.divideTeams();
+    system("CLS");
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
