cee9b02ce6014afae4410ed34c9545f92f595a16
Cactus-John <ivan.ivo.bosnjak@gmail.com>
2022-10-20 23:16:30 +0200

    Update RandomTeamGenerator.cpp

--- RandomTeamGenerator/RandomTeamGenerator.cpp
+++ RandomTeamGenerator/RandomTeamGenerator.cpp
@@ -1,39 +1,88 @@
 
 #include <iostream>
 #include <string>
-#include <cstdlib>
 #include <time.h>
 #include <algorithm>
 using namespace std;
 
 class randomTeamGenerator {
 
-    public:
-        int playerNumbers, teamNumbers;
-        string players;
+public:
+    int playerNumbers{}, teamNumbers{};
+    string players;
 
-    // user writes all the players in his team
+    // user writes all the players in both teams
     void teamMembers() {
-        string* players = new string[playerNumbers];
-        for (int i = 0; i < playerNumbers ; i++) {
+        string players[50];
+        for (int i = 0; i < playerNumbers; i++) {
             cin >> players[i];
         }
     }
 
-    void divideTeams() {
-        srand(time(NULL));
-        int rndm = rand() % playerNumbers + 1;
-        int i = 0;
-        while (i <= playerNumbers) {
-            cout << players[rndm] << endl;
-            if (i % teamNumbers == 0) {
-                cout << endl;
-                i++;
+    // All players are divided into how many teams the user wants
+    /*
+       divide the players into teams: if the number of teams is even, divide the number of players by the number of teams to see if there is any remainder, we need to check aswell
+       if both the number of teams and the number of players are greater or less than one another*/
+
+    // if the number of teams is odd, divide the number of players by the number of teams and add the remaining 1 
+    public: void divideTeams() {
+
+        for (int j = 0; j < playerNumbers; j++) {
+
+            if (teamNumbers % 2 == 0 && playerNumbers % 2 == 0) {
+
+                if (playerNumbers % teamNumbers == 0 && playerNumbers > teamNumbers) {
+                    players[j] = playerNumbers / teamNumbers;
+                    cout << players[j];
+                }
+
+                if (playerNumbers < teamNumbers) {
+                    exit(-1);
+                }
             }
-        }    
+
+            else if (teamNumbers % 2 != 0 && playerNumbers % 2 == 0) {
+
+                if (playerNumbers % teamNumbers == 0 && playerNumbers > teamNumbers) {
+                    players[j] = playerNumbers / teamNumbers;
+                    cout << players[j];
+                }
+
+                if (playerNumbers < teamNumbers) {
+                    exit(-1);
+                }
+
+            }
+
+            else if (teamNumbers % 2 == 0 && playerNumbers % 2 != 0) {
+
+                if (playerNumbers % teamNumbers == 0 && playerNumbers > teamNumbers) {
+                    players[j] = playerNumbers / teamNumbers;
+                    cout << players[j];
+                }
+
+                if (playerNumbers < teamNumbers) {
+                    exit(-1);
+                }
+            }
+
+            else if (teamNumbers % 2 != 0 && playerNumbers % 2 != 0) {
+
+                if (playerNumbers % teamNumbers == 0 && playerNumbers > teamNumbers) {
+                    players[j] = playerNumbers / teamNumbers;
+                    cout << players[j];
+                }
+
+                if (playerNumbers < teamNumbers) {
+                    exit(-1);
+                }
+            }
+            else
+                exit(-1);
+        }
     }
 };
-
+ 
 int main() {
 
     randomTeamGenerator names;
@@ -44,6 +93,7 @@ int main() {
     cout << "How many teams you want to have? : " << endl; cin >> names.teamNumbers;
  
     names.divideTeams();
-    system("CLS");
+
+    return 0;
 }
 
