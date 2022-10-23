5f369c25aa1a25122f20e2e78cc5e452f8217c9b
Cactus-John <ivan.ivo.bosnjak@gmail.com>
2022-10-23 16:49:46 +0200

    generate the random order of entered strings (1)

--- RandomTeamGenerator/RandomTeamGenerator.cpp
+++ RandomTeamGenerator/RandomTeamGenerator.cpp
@@ -1,99 +1,73 @@
 
 #include <iostream>
-#include <string>
+#include <cstdlib>
 #include <time.h>
 #include <algorithm>
 using namespace std;
 
 class randomTeamGenerator {
 
-public:
-    int playerNumbers{}, teamNumbers{};
-    string players;
+    public:
+        int playerNumbers{}, teamNumbers{};
+        string players;
 
     // user writes all the players in both teams
-    void teamMembers() {
-        string players[50];
-        for (int i = 0; i < playerNumbers; i++) {
-            cin >> players[i];
-        }
-    }
 
     // All players are divided into how many teams the user wants
     /*
        divide the players into teams: if the number of teams is even, divide the number of players by the number of teams to see if there is any remainder, we need to check aswell
        if both the number of teams and the number of players are greater or less than one another*/
 
-    // if the number of teams is odd, divide the number of players by the number of teams and add the remaining 1 
-    public: void divideTeams() {
+       // if the number of teams is odd, divide the number of players by the number of teams and add the remaining 1 
+    void divideTeams() {
+        string players[50];
+        for (int i = 0; i < playerNumbers; i++)
+            cin >> players[i];
 
-        for (int j = 0; j < playerNumbers; j++) {
-
-            if (teamNumbers % 2 == 0 && playerNumbers % 2 == 0) {
-
-                if (playerNumbers % teamNumbers == 0 && playerNumbers > teamNumbers) {
-                    players[j] = playerNumbers / teamNumbers;
-                    cout << players[j];
-                }
-
-                if (playerNumbers < teamNumbers) {
-                    exit(-1);
-                }
+        /*if (playerNumbers % teamNumbers == 0 && playerNumbers > teamNumbers) {
+            count = playerNumbers / teamNumbers;
+            cout << endl;
+            for (int i = 0; i < count; i++) {
+                cout << players[i] << endl;
             }
-
-            else if (teamNumbers % 2 != 0 && playerNumbers % 2 == 0) {
-
-                if (playerNumbers % teamNumbers == 0 && playerNumbers > teamNumbers) {
-                    players[j] = playerNumbers / teamNumbers;
-                    cout << players[j];
-                }
-
-                if (playerNumbers < teamNumbers) {
-                    exit(-1);
-                }
-
+    
+            cout << "--------" << endl;
+            for (int i = count; i < playerNumbers; i++) {
+                cout << players[i] << endl;
             }
+        }*/
 
-            else if (teamNumbers % 2 == 0 && playerNumbers % 2 != 0) {
-
-                if (playerNumbers % teamNumbers == 0 && playerNumbers > teamNumbers) {
-                    players[j] = playerNumbers / teamNumbers;
-                    cout << players[j];
-                }
-
-                if (playerNumbers < teamNumbers) {
-                    exit(-1);
-                }
+        int random = rand() % (playerNumbers / teamNumbers) + 1;
+        if (playerNumbers % teamNumbers == 0 && playerNumbers > teamNumbers) {
+            cout << endl;
+            for (int i = 0; i < teamNumbers; i++) {
+                cout << players[random] << endl;
+                random = rand() % (playerNumbers / teamNumbers) + 1;
+                random++;
             }
+             
+            cout << "--------" << endl;
 
-            else if (teamNumbers % 2 != 0 && playerNumbers % 2 != 0) {
-
-                if (playerNumbers % teamNumbers == 0 && playerNumbers > teamNumbers) {
-                    players[j] = playerNumbers / teamNumbers;
-                    cout << players[j];
-                }
-
-                if (playerNumbers < teamNumbers) {
-                    exit(-1);
-                }
+            for (int i = teamNumbers; i < playerNumbers; i++) {
+                cout << players[random] << endl;
+                random = rand() % (playerNumbers / teamNumbers) + 1;
+                random++;
             }
-            else
-                exit(-1);
         }
     }
 };
- 
+
 int main() {
 
+    srand(time(nullptr));
     randomTeamGenerator names;
-    cout << "Enter the number of players: " << endl;
-    cout << "Number of players: "; cin >> names.playerNumbers; 
+    cout << "Number of players: "; cin >> names.playerNumbers;
+    cout << "How many teams you want to have? : "; cin >> names.teamNumbers;
     system("CLS");
-    cout << "Write the names of the players: " << endl;  names.teamMembers();
-    cout << "How many teams you want to have? : " << endl; cin >> names.teamNumbers;
- 
-    names.divideTeams();
+    cout << "Write the names of the players: " << endl;  names.divideTeams();
 
+    if (names.playerNumbers < names.teamNumbers)
+        exit(-1);
+    
     return 0;
 }
-
